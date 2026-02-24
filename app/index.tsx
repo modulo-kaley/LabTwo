8012c2036869b6413b4644d8b8c1be66b3530397
Kaley <kaleyywood@gmail.com>
2026-02-23 21:35:20 -0700

    index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,54 @@
-import { Text, View } from "react-native";
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
+import ToDoForm from "./ToDoForm";
+import ToDoList from "./ToDoList";
 
+// Each task has an id, text, and completed boolean 
 export default function Index() {
+  const [tasks, setTasks ] = useState ([
+    { id: 1, text: 'Do laundry' , completed: true },
+    { id: 2, text: 'Go to gym' , completed: false },
+    { id: 3, text: 'Walk dog' , completed: true },
+  ]);
+
+  // Tracks what the user is typing in the input field
+  const [input, setInput] = useState('');
+
+  function handleAddTask() {
+    // If the input is empty, do nothing
+    if (input.trim() === '') return;
+
+    /*
+    * Spread the tasks in a new array 
+    * Append the new task 
+    * Date.now() = unique id
+     */
+    setTasks([...tasks, { id: Date.now(), text: input, completed: false}]);
+    
+    // clear the input field after adding 
+    setInput('');
+  }
+
+  /* Toggles the completed boolean 
+  * finds task by id 
+  */
+  function handleToggleTask(id: number) {
+    setTasks(tasks.map((task) =>
+    task.id === id ? {...task, completed: !task.completed } : task
+    ))
+  }
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ToDoList tasks={tasks} onToggle={handleToggleTask} />
+
+      <ToDoForm input={input} onChangeText={setInput} onAdd={handleAddTask} />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
